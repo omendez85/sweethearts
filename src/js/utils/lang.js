dcde199cacfadeac8b293b6c4fcc816994e9914c
Oscar Mendez <omendez85@gmail.com>
2017-10-22 11:39:46 -0500

    Revert "Core: replace Mustache by Twig module"
    This reverts commit 7710c0776203ae74de6eb7048b9dd3e403764ca6.

--- src/js/utils/lang.js
+++ src/js/utils/lang.js
@@ -1,3 +1,14 @@
+// const lang = [
+//     {
+//         id: "mainTitle",
+//         lang: {
+//             es: "Cual princesa de Disney eres tu?",
+//             en: "Which Disney Princess Are You?",
+//             ge: "Welche Disney Princess bist du?"
+//         }
+//     }
+// ];
+
 const lang = {
     mainTitle: {
         es: "Cual princesa de Disney eres tu?",
