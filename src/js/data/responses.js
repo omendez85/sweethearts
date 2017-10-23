314f687715aa1f0c9fcf1277a77d03d9859d539e
Oscar Mendez <omendez85@gmail.com>
2017-10-23 15:54:42 -0500

    Feat: Add result page

---
+++ src/js/data/responses.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        princessName: "Bella",
+        urlImg: "/assets/img/bella.png",
+        responses: [
+            [3,2,3,1,1,1],
+            [3,3,3,3,1,2]
+        ]
+    },
+    {
+        princessName: "Snowwhite",
+        urlImg: "/assets/img/snowwhite.png",
+        responses: [
+            [2,1,2,2,2,1],
+            [2,3,2,3,2,3]
+        ]
+    },
+    {
+        princessName: "Merida",
+        urlImg: "/assets/img/merida.png",
+        responses: [
+            [1,1,3,1,1,2],
+            [1,5,1,3,1,3]
+        ]
+    },
+    {
+        princessName: "Fiona",
+        urlImg: "/assets/img/fiona.png",
+        responses: [
+            [3,4,3,1,1,1],
+            [3,5,3,3,1,2]
+        ]
+    },
+    {
+        princessName: "Cinderella",
+        urlImg: "/assets/img/cinderella.png",
+        responses: [
+            [2,1,1,1,2,1],
+            [2,4,1,3,2,3]
+        ]
+    },
+    {
+        princessName: "Ariel",
+        urlImg: "/assets/img/ariel.png",
+        responses: [
+            [1,1,1,1,3,2],
+            [1,6,1,2,3,3]
+        ]
+    },
+]
--- src/js/utils/lang.js
+++ src/js/utils/lang.js
@@ -133,6 +133,22 @@ const lang = {
         de: "Lilien"
     },
 
+    loading: {
+        es: "Calculando... ",
+        en: "Calculating...",
+        de: "Berechnung..."
+    },
+    resultPrincess: {
+        es: "Genial, luces como: ",
+        en: "Awesome, you looks like:",
+        de: "Ehrfürchtig, du siehst aus wie:"
+    },
+    resultPrincess2: {
+        es: "No olvides que eres y siempre seras una princesa.",
+        en: "Do not forget that you are and you will always be a princess.",
+        de: "Vergiss nicht, dass du es bist und du wirst immer eine Prinzessin sein."
+    }
+
 };
 
 export default lang;
